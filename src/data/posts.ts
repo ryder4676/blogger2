94a91760720ad5ef89cb5bc075d57d6cc818193e
Jayson Ronald <ryder4676@byui.edu>
2024-02-17 19:24:26 +0000

    Added posts.ts

---
+++ src/data/posts.ts
@@ -0,0 +1,39 @@
+export const posts = [
+  {
+    id: 1,
+    title: 'Blog Post 1',
+    content: 'this is content post',
+    username: 'john123',
+  },
+  {
+    id: 2,
+    title: 'Blog Post 2',
+    content: 'this is content post',
+    username: 'john123',
+  },
+  {
+    id: 3,
+    title: 'Blog Post 3',
+    content: 'this is content post',
+    username: 'john123',
+  },
+  {
+    id: 4,
+    title: 'Blog Post 4',
+    content: 'this is content post',
+    username: 'john123',
+  },
+  {
+  id: 5,
+  title: 'Blog Post 5',
+  content: 'this is content post',
+  username: 'john12345',
+  },
+  {
+    id: 6,
+    title: 'Blog Post 6',
+    content: 'this is content post',
+    username: 'john123456',
+    }
+  
+];
